9aa2c6b38f03806283abe3b1b76aad42e7c78e36
Annamaria Bastian <annie_m29@yahoo.com>
2013-03-13 12:25:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RampupProject::Application.config.secret_token = 'd20a22ac008ca60b67b052b1301e9ea7aa842c4713e2622fdc0d9183b7754a04993b51345e493e17958518d415968e3c30a077b0930ca858045e1d213c6c8118'
